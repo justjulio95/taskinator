730af50ac384ab8563b6baa9db9f930aeb79f846
Julio.S <just.julio95@gmail.com>
2022-03-24 15:22:34 -0400

    add user-made list item

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,30 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
     //Prevents the automatic refresh that the browser wants to do
     event.preventDefault();
-    //Creates a new task item.
+
+    //Assigns the user input to the taskNameInput variable
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    //Assigns the user task selection to the taskTypeInput variable
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    //Creates list item.
     var listItemEl = document.createElement("li");
     //Adopts the CSS class name AND style
     listItemEl.className = "task-item";
+
+    // Create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    //Give it a class name
+    taskInfoEl.className = "task-info";
+    //Add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + 
+    "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
     //Adds the text
-    listItemEl.textContent = "This is a new task.";
-    //Appends element to the task list
+    listItemEl.appendChild(taskInfoEl);
+
+    //Appends entire list item to list
     tasksToDoEl.appendChild(listItemEl);
+    console.dir(listItemEl);
 }
 
 formEl.addEventListener("submit", createTaskHandler)
